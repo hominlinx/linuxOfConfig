61ae292de0eeed9abf7d5a6eeb09e95aa6a8551b
hominlinx <monkeyyuan635@gmail.com>
2013-09-05 14:35:31 +0800

    modified tmux.conf

--- makeFile.sh
+++ makeFile.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 cd .
 
+BIN=$HOME/bin
 CF_DIR=`pwd`
+
+if [ ! -d $BIN ]; then
+    mkdir $BIN
+fi
+
 #for tmux
+echo "Installing tmuxen to $bin"
+ln -sf $CF_DIR/config/tmuxen $BIN/tmuxen
+
+echo "Symlinking .tmux.conf to $HOME/.tmux.conf"
 ln -s $CF_DIR/config/tmux.conf ~/.tmux.conf
 echo "alias tmux='tmux -2'" >> ~/.bashrc
 source .bash_profile

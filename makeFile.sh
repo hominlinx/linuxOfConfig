cbf691efbf1462246f1aced8ca5b8c2b55fd362d
yuanguang <yuanguang@pset.suntec.net>
2014-04-02 16:05:30 +0800

    add tmux fold

--- makeFile.sh
+++ makeFile.sh
@@ -34,9 +34,9 @@ sh -x $CF_DIR/vim/install.sh
 
 #for tmux
 echo "Installing tmux  colors to $BIN"
-ln -s $CF_DIR/config/.tmuxcolors.conf ~/.tmuxcolors.conf
+ln -s $CF_DIR/tmux/.tmuxcolors.conf ~/.tmuxcolors.conf
 
 echo "Symlinking .tmux.conf to $HOME/.tmux.conf"
-ln -s $CF_DIR/config/tmux.conf ~/.tmux.conf
+ln -s $CF_DIR/tmux/tmux.conf ~/.tmux.conf
 echo "alias tmux='tmux -2'" >> ~/.bashrc
 #source .bash_profile
